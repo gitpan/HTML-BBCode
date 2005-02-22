28fea37a2389052c1721821d251f9d400c3e761c
Menno Blom <blom@cpan.org>
2005-02-22 16:43:18 +0000

    Import of BLOM/HTML-BBCode-1.00 from CPAN.
    gitpan-cpan-distribution: HTML-BBCode
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         BLOM/HTML-BBCode-1.00.tar.gz
    gitpan-cpan-author:       BLOM
    gitpan-cpan-maturity:     released

--- lib/HTML/BBCode.pm
+++ lib/HTML/BBCode.pm
@@ -11,23 +11,27 @@ HTML::BBCode - Perl extension for converting BBcode to HTML.
   my $bbc  = HTML::BBCode->new( \%options );
   my $html = $bbc->parse($bbcode);
 
+  # Input
+  print $bbc->{bbcode};
+
+  # Output
+  print $bbc->{html};
+
 =head1 DESCRIPTION
 
 C<HTML::BBCode> converts BBCode -as used on the phpBB bulletin
-boards- to it's HTML equivalent.
+boards- to its HTML equivalent.
 
 =head2 METHODS
 
 The following methods can be used
 
 =head3 new
-   
+
    my $bbc = HTML::BBCode->new({
       allowed_tags => [ @bbcode_tags ],
-      html_tags    => \%html_tags, 
-      match        => \%match, 
-      substitute   => \%substitute,
-      no_html	   => 1,
+      html_tags    => \%html_tags,
+      no_html      => 1,
       linebreaks   => 1,
    });
 
@@ -51,14 +55,6 @@ Configures the wanted output in HTML. Defaults to (almost) the same as
 used on the phpbb bulletin boards (<b>, <u> etc. have been turned into
 their CSS equivalents).
 
-=item match
-
-Specifies the regexp needed to catch the BBCode.
-
-=item substitute
-
-Specifies the substitute command for the C<match> regexp.
-
 =item no_html
 
 Disabled by default.
@@ -75,15 +71,14 @@ When true, will substitute linebreaks into HTML ('<br />')
 
 =head3 parse
 
-   my $html = $bbc->parse($bbcode); 
+   my $html = $bbc->parse($bbcode);
 
-Parses text supplied as a single scalar string and returns the HTML as 
+Parses text supplied as a single scalar string and returns the HTML as
 a single scalar string.
 
 =head1 SEE ALSO
 
-http://www.b10m.net/cgi-bin/HTML-BBCode.cgi
-http://www.phpbb.com/phpBB/faq.php?mode=bbcode
+http://www.b10m.net/cgi-bin/HTML-BBCode.cgi http://www.phpbb.com/phpBB/faq.php?mode=bbcode
 
 =head1 BUGS
 
@@ -105,9 +100,8 @@ it under the same terms as Perl itself.
 #------------------------------------------------------------------------------#
 use strict;
 use warnings;
-use Data::Dumper;
 
-our $VERSION = '0.04';
+our $VERSION = '1.00';
 our @bbcode_tags = qw(code quote b u i color size list url email img);
 
 sub new {
@@ -130,9 +124,9 @@ sub _init {
                    '<div class="bbcode_code_body">%s</div>',
      quote      => '<div class="bbcode_quote_header">%s</div>'.
                    '<div class="bbcode_quote_body">%s</div>',
-     b          => '<span style="font-weight: bold">%s</span>', 
+     b          => '<span style="font-weight: bold">%s</span>',
      u          => '<span style="text-decoration: underline;">%s</span>',
-     i          => '<span style="font-style: italic">%s</span>', 
+     i          => '<span style="font-style: italic">%s</span>',
      color      => '<span style="color: %s">%s</span>',
      size       => '<span style="font-size: %spx">%s</span>',
      url        => '<a href="%s">%s</a>',
@@ -143,88 +137,200 @@ sub _init {
      ol_alpha   => '<ol style="list-style-type: lower-alpha;">%s</ol>',
    );
 
-   my %match = (
-     code  => qr|\[code\](.+?)\[/code\]|iso,
-     quote => qr|\[quote(="([^"]+)")?\](.+?)\[/quote\]|iso,
-     b     => qr|\[b\](.+?)\[/b\]|iso,
-     u     => qr|\[u\](.+?)\[/u\]|iso,
-     i     => qr|\[i\](.+?)\[/i\]|iso,
-     color => qr|\[color=(.+?)\](.+?)\[/color\]|iso,
-     size  => qr|\[size=(.+?)\](.+?)\[/size\]|iso,
-     url   => qr|\[url[=]?([^\]]+?)?\](.+?)\[/url\]|iso,
-     email => qr|\[email\](.+?)\[/email\]|iso,
-     img   => qr|\[img\](.+?)\[/img\]|iso,
-     list  => qr|\[list[=]?([^\]]+?)?\](.+?)\[/list\]|iso,
-   );
-
-   my %substitute = (
-     code  => 'sprintf($html_tags{code},&_code($1))',
-     quote => 'sprintf($html_tags{quote},($2)?"$2 wrote:":"Quote:", $3)',
-     b     => 'sprintf($html_tags{b},$1)',
-     u     => 'sprintf($html_tags{u},$1)',
-     i     => 'sprintf($html_tags{i},$1)',
-     color => 'sprintf($html_tags{color}, $1, $2)',
-     size  => 'sprintf($html_tags{size}, $1, $2)',
-     url   => 'sprintf($html_tags{url},($1)?$1:$2, $2)',
-     email => 'sprintf($html_tags{email}, $1, $1)',
-     img   => 'sprintf($html_tags{img}, $1)',
-     list  => '&_list($self, $1, $2)',
-   );
-
-   my %options = ( 
-                  allowed_tags => [ @bbcode_tags ],
-                  html_tags    => \%html_tags, 
-                  match        => \%match, 
-                  substitute   => \%substitute, 
-                  no_html      => 0,
-                  linebreaks   => 0,
-                  %{ $args },
-                 );
-
+   my %options = (
+		  allowed_tags=> \@bbcode_tags,
+		  html_tags    => \%html_tags,
+		  no_html      => 0,
+		  linebreaks   => 0,
+ 		  %{ $args },
+		);
    $self->{options} = \%options;
-
    return $self;
 }
 
+# Parse the input!
 sub parse {
    my ($self, $bbcode) = @_;
-   $self->{bbcode} = $bbcode; 
-   $self->{html} = $bbcode; 
+   $self->{_stack} = ();
+   $self->{_in_code_block} = 0;
+   $self->{_skip_nest} = '';
+   $self->{_nest_count} = 0;
+   $self->{_nest_count_stack} = 0;
+   $self->{_dont_nest} = ['code', 'url', 'email', 'img'];
+   $self->{bbcode} = '';
+   $self->{html} = '';
 
-   my %match      = %{ $self->{options}->{match} };
-   my %substitute = %{ $self->{options}->{substitute} };
-   my %html_tags  = %{ $self->{options}->{html_tags} };
+   $self->{bbcode} = $bbcode;
+   my $input = $bbcode;
 
-   # "Strip" HTML input
+   main:
+   while(1) {
+      # End tag
+      if($input =~ /^(\[\/[^\]]+\])/s) {
+         my $end = $1;
+	 if($self->{_skip_nest} ne '' && $end ne "[/$self->{_skip_nest}]") {
+            _content($self, $end);
+	 } else {
+            _end_tag($self, $end);
+	 }
+         $input = $';
+      }
+
+      # Opening tag
+      elsif($input =~ /^(\[[^\]]+\])/s ) {
+         _open_tag($self, $1);
+         $input = $';
+      }
+
+      # None BBCode content
+      elsif($input =~ /^([^\[]+)/s) {
+         _content($self, $1);
+         $input = $';
+      }
+
+      # Now what?
+      else {
+         last main if(!$input);	# We're at the end now, stop parsing!
+      }
+   }
+   $self->{html} = join(' ', @{$self->{_stack}});
+   return $self->{html};
+}
+
+sub _open_tag {
+   my ($self, $open) = @_;
+   my ($tag) = $open =~ m/\[([^=\]]+).*?\]/s;	# Don't do this! ARGH!
+   if(_dont_nest($self, $tag)) {
+      $self->{_skip_nest} = $tag;
+   }
+   if($self->{_skip_nest} eq $tag) {
+      $self->{_nest_count}++;
+      $self->{_nest_count_stack}++;
+   }
+   $self->{_in_code_block}++ if($tag eq 'code');
+   push @{$self->{_stack}}, $open;
+}
+
+sub _content {
+   my ($self, $content) = @_;
    if($self->{options}->{no_html}) {
-      $self->{html} =~ s|<|&lt;|gs;
-      $self->{html} =~ s|>|&gt;|gs;
+      $content =~ s|<|&lt;|gs;
+      $content =~ s|>|&gt;|gs;
+   }
+   $content =~ s|\n|<br />\n|gs if($self->{options}->{linebreaks} &&
+   			           $self->{_in_code_block} == 0);
+   push @{$self->{_stack}}, $content;
+}
+
+sub _end_tag {
+   my ($self, $end) = @_;
+   my ($tag, $arg);
+   my @buf = ( $end );
+
+   if("[/$self->{_skip_nest}]" eq $end && $self->{_nest_count} > 1) {
+      push @{$self->{_stack}}, $end;
+      $self->{_nest_count}--;
+      return;
    }
 
-   # Substitute linebreaks
-   $self->{html} =~ s|\n|<br />\n|gs if($self->{options}->{linebreaks});
-  
-   # Substitute the BBCode
-   map { 
-         if(_is_allowed($self, $_)) {
-          $self->{html} =~ s|$match{$_}|$substitute{$_}|eegis
-         }
-       } @bbcode_tags;
+   $self->{_in_code_block} = 0 if($end eq '[/code]');
+	 
+   # Loop through the stack
+   while(1) {
+         my $item = pop(@{$self->{_stack}});
+         push @buf, $item;
 
-   return $self->{html};
+         if(!defined $item) {
+	    map { push @{$self->{_stack}}, $_ if($_) } reverse @buf;
+	    last;
+         }
+
+	 
+         if("[$self->{_skip_nest}]" eq "$item") {
+	       $self->{_nest_count_stack}--;
+	       next if($self->{_nest_count_stack} > 0);
+	 }
+
+         $self->{_nest_count}--
+            if("[/$self->{_skip_nest}]" eq $end && $self->{_nest_count} > 0) ;
+
+
+	 if($item =~ /\[([^=\]]+).*\]/s) {
+            $tag = $1;
+	    if ($tag && $end eq "[/$tag]") {
+               push @{$self->{_stack}}, (_is_allowed($self, $tag))
+	       				? _do_BB($self, @buf)
+					: reverse @buf;
+	       last;
+            }
+         }
+   }
+}
+
+sub _do_BB {
+   my ($self, @buf) = @_;
+   my ($tag, $attr);
+   my $html;
+
+   # Get the opening tag
+   my $open = pop(@buf); 
+   # We prefer to read in non-reverse way
+   @buf = reverse @buf;
+   # Closing tag is kinda useless, pop it
+   pop(@buf);
+   # Rest should be content;
+   my $content = join(' ', @buf);
+
+   # What are we dealing with anyway? Any attributes maybe?
+   if($open =~ /\[([^=\]]+)=?([^\]]+)?]/) {
+      $tag  = $1;
+      $attr = $2;
+   }
+
+   # Kludgy way to handle specific BBCodes ... 
+   if($tag eq 'quote') {
+      $html = sprintf($self->{options}->{html_tags}->{quote},
+                        ($attr) ? "$attr wrote:"
+                              : "Quote:",
+                        $content  
+                     );
+   } elsif($tag eq 'code') {
+     $html = sprintf($self->{options}->{html_tags}->{code}, _code($content));
+   } elsif($tag eq 'list') {
+     $html = _list($self, $attr, $content);
+   } elsif(($tag eq 'email' || $tag eq 'url') && !$attr) {
+      $html = sprintf($self->{options}->{html_tags}->{$tag}, $content,$content);
+   } elsif ($attr) {
+      $html = sprintf($self->{options}->{html_tags}->{$tag}, $attr, $content);
+   } else {
+      $html = sprintf($self->{options}->{html_tags}->{$tag}, $content);
+   }
+   # Clear the _skip_nest?
+   $self->{_skip_nest} = '' if(defined $self->{_skip_nest} &&
+                               $tag eq $self->{_skip_nest});
+   # Return ...
+   return split(' ', $html);
 }
 
 sub _is_allowed {
    my ($self, $check) = @_;
-   map { 
-         return 1 if ($_ eq $check); 
+   map {
+         return 1 if ($_ eq $check);
        } @{$self->{options}->{allowed_tags}};
-   return 0; 
+   return 0;
+}
+
+sub _dont_nest {
+   my ($self, $check) = @_;
+   map {
+   	 return 1 if($_ eq $check);
+       } @{$self->{_dont_nest}};
+   return 0;
 }
 
 sub _code {
    my $code = shift;
-   $code =~ s|^\n||;
+   $code =~ s|^\s+?[\n\r]+?||;
    $code =~ s|<|\&lt;|g;
    $code =~ s|>|\&gt;|g;
    $code =~ s|\[|\&#091;|g;
@@ -235,18 +341,16 @@ sub _code {
 }
 
 sub _list {
-   my ($self, $type, $list) = @_;
-   my $html;
-   if($type) {
-      $html = $self->{options}->{html_tags}->{ol_number} if($type =~ m|^\d|);
-      $html = $self->{options}->{html_tags}->{ol_alpha} if($type =~ m|^\D|i);
+   my ($self, $attr, $content) = @_;
+   $content =~ s|\[\*\]([^(\[]+)|<li>$1</li>|gs;
+   if($attr) {
+      return sprintf($self->{options}->{html_tags}->{ol_number}, $content)
+         if($attr =~ /^\d/);
+      return sprintf($self->{options}->{html_tags}->{ol_alpha}, $content)
+         if($attr =~ /^\D/);
+   } else {
+      return sprintf($self->{options}->{html_tags}->{ul}, $content);
    }
-   else {
-      $html = $self->{options}->{html_tags}->{ul}; 
-   }
-
-   $list =~ s|\[\*\]([^\[]+)|<li>$1</li>|gs;
-   return sprintf($html, $list);
 }
 
 sub _croak {
@@ -255,5 +359,5 @@ sub _croak {
     Carp::croak(@error);
 }
 
+
 1;
-__END__
--- t/HTML-BBCode.t
+++ t/HTML-BBCode.t
@@ -1,6 +1,6 @@
 #########################
 
-use Test::More tests => 25;
+use Test::More tests => 23;
 BEGIN { use_ok 'HTML::BBCode'; }
 
 #########################
@@ -18,33 +18,26 @@ is($bbc->parse('[i]foo[/i]'), '<span style="font-style: italic">foo</span>', 'it
 is($bbc->parse('[color=red]foo[/color]'), '<span style="color: red">foo</span>', 'color');
 is($bbc->parse('[size=24]foo[/size]'),  '<span style="font-size: 24px">foo</span>', 'font size');
 is($bbc->parse('[quote]foo[/quote]'), '<div class="bbcode_quote_header">Quote:</div><div class="bbcode_quote_body">foo</div>', 'quote (simple)');
-is($bbc->parse('[quote="foo"]bar[/quote]'), '<div class="bbcode_quote_header">foo wrote:</div><div class="bbcode_quote_body">bar</div>', 'quote (with author)');
+is($bbc->parse('[quote="foo"]bar[/quote]'), '<div class="bbcode_quote_header">"foo" wrote:</div><div class="bbcode_quote_body">bar</div>', 'quote (with author)');
 is($bbc->parse('[code]<foo>[/code]'), '<div class="bbcode_code_header">Code:</div><div class="bbcode_code_body">&lt;foo&gt;</div>', 'code');
 is($bbc->parse('[url]http://www.b10m.net/[/url]'), '<a href="http://www.b10m.net/">http://www.b10m.net/</a>', 'hyperlink (simple)');
 is($bbc->parse('[url=http://www.b10m.net/]lame site[/url]'), '<a href="http://www.b10m.net/">lame site</a>', 'hyperlink (with link-text)');
 is($bbc->parse('[email]foo@bar.com[/email]'), '<a href="mailto:foo@bar.com">foo@bar.com</a>', 'mailto-links');
 is($bbc->parse('[img]foo.png[/img]'), '<img src="foo.png" />', 'image');
-is($bbc->parse('[list][*]foo[*]bar[/list]'), '<ul><li>foo</li><li>bar</li></ul>', 'unordered list');
-is($bbc->parse('[list=1][*]foo[*]bar[/list]'), '<ol><li>foo</li><li>bar</li></ol>', 'ordered list');
-is($bbc->parse('[list=a][*]foo[*]bar[/list]'), '<ol style="list-style-type: lower-alpha;"><li>foo</li><li>bar</li></ol>', 'ordered list (alpha style)');
+is($bbc->parse('[list][*]foo[*]bar[/list]'), '<ul><li> foo </li><li> bar</li></ul>', 'unordered list');
+is($bbc->parse('[list=1][*]foo[*]bar[/list]'), '<ol><li> foo </li><li> bar</li></ol>', 'ordered list');
+is($bbc->parse('[list=a][*]foo[*]bar[/list]'), '<ol style="list-style-type: lower-alpha;"><li> foo </li><li> bar</li></ol>', 'ordered list (alpha style)');
 
 # Mix them and do 'em wrong!
-is($bbc->parse('[b]bold, [i]bold and italic[/i][/b][/b]'), '<span style="font-weight: bold">bold, <span style="font-style: italic">bold and italic</span></span>[/b]', 'mixed, and "wrong"');
+is($bbc->parse('[b]bold, [i]bold and italic[/i][/b][/b]'), '<span style="font-weight: bold">bold, <span style="font-style: italic">bold and italic</span></span> [/b]', 'mixed, and "wrong"');
 
-# new object, with options
-$bbc = HTML::BBCode->new({ allowed_tags => [qw(email quote)] });
-isa_ok($bbc, 'HTML::BBCode', 'default');
-
-# Nested tags
-is($bbc->parse('[quote="[email]b10m@perlmonk.org[/email]"]foo[/quote]'), '<div class="bbcode_quote_header"><a href="mailto:b10m@perlmonk.org">b10m@perlmonk.org</a> wrote:</div><div class="bbcode_quote_body">foo</div>', 'nested tags');
-
-# Nested tags with disallowed tag
-is($bbc->parse('[quote="[email]b10m@perlmonk.org[/email]"][b]foo[/b][/quote]'), '<div class="bbcode_quote_header"><a href="mailto:b10m@perlmonk.org">b10m@perlmonk.org</a> wrote:</div><div class="bbcode_quote_body">[b]foo[/b]</div>', 'nested tags (w/ disallowed tag)');
+# Nested quotes
+is($bbc->parse('[quote="world population"][quote="B10m"]no one listens[/quote]you\'ve got that right[/quote]boohoo'), '<div class="bbcode_quote_header">"world population" wrote:</div><div class="bbcode_quote_body"><div class="bbcode_quote_header">"B10m" wrote:</div><div class="bbcode_quote_body">no one listens</div> you\'ve got that right</div> boohoo', 'nested quotes');
 
 # new object, with no_html
 $bbc = HTML::BBCode->new({ no_html => 1 });
 isa_ok($bbc, 'HTML::BBCode', 'default');
-is($bbc->parse('<i>[b]bold[/b]</i>'), '&lt;i&gt;<span style="font-weight: bold">bold</span>&lt;/i&gt;', 'no_html');
+is($bbc->parse('<i>[b]bold[/b]</i>'), '&lt;i&gt; <span style="font-weight: bold">bold</span> &lt;/i&gt;', 'no_html');
 
 # new object, with linebreak
 $bbc = HTML::BBCode->new({ linebreaks => 1 });
@@ -54,4 +47,4 @@ This is a test to see
 wheter linebreaks can be converted.
 __TEXT__
 
-is($bbc->parse($test), "This is a test to see<br />\nwheter linebreaks can be converted.<br />\n");
+is($bbc->parse($test), "This is a test to see<br />\nwheter linebreaks can be converted.<br />\n", 'linebreaks');
